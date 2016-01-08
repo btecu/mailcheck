ab8bfe11fb0898ee1538751c58880f86e89ab377
Bujorel Tecu <btecu@vikus.com>
2016-01-08 09:20:34 -0500

    Remove domains - use passed in list instead

--- src/mailcheck.js
+++ src/mailcheck.js
@@ -13,21 +13,11 @@ var Mailcheck = {
   secondLevelThreshold: 2,
   topLevelThreshold: 2,
 
-  defaultDomains: [
-     'aim.com', 'aol.com', 'att.net', 'bellsouth.net', 'btinternet.com',
-     'charter.net', 'comcast.net', 'cox.net', 'earthlink.net', 'gmail.com',
-     'google.com', 'googlemail.com', 'icloud.com', 'mac.com', 'me.com',
-     'msn.com', 'optonline.net', 'optusnet.com.au', 'qq.com', 'rocketmail.com',
-     'rogers.com', 'sbcglobal.net', 'shaw.ca', 'sky.com', 'sympatico.ca',
-     'telus.net', 'verizon.net', 'web.de', 'xtra.co.nz', 'ymail.com'],
+  defaultDomains: ['gmail.com'],
 
-  defaultSecondLevelDomains: ['gmx', 'hotmail', 'live', 'mail', 'outlook', 'yahoo'],
+  defaultSecondLevelDomains: [ 'gmx', 'hotmail', 'live', 'mail', 'outlook', 'yahoo'],
 
-  defaultTopLevelDomains: [
-    'at', 'be', 'biz', 'ca', 'ch', 'co.il', 'co.jp', 'co.nz', 'co.uk',
-    'com', 'com.au', 'com.tw', 'cz', 'de', 'dk', 'edu', 'es', 'eu', 'fr',
-    'gov', 'gr', 'hk', 'hu', 'ie', 'in', 'info', 'it', 'jp', 'kr', 'mil',
-    'net', 'net.au', 'nl', 'no', 'org', 'ru', 'se', 'sg', 'us'],
+  defaultTopLevelDomains: ['at', 'ca', 'co.uk', 'de', 'edu', 'es', 'fr', 'net', 'org', 'us', 'com'],
 
   run: function(opts) {
     opts.domains = opts.domains || Mailcheck.defaultDomains;
